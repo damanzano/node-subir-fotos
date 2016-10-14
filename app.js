a9aacc59fd99f90747a6c38e1f720960cc4d0ba5
damanzano <damh24@gmail.com>
2016-10-14 13:59:30 -0500

    Agrega algunos cambios de configuración para aceptar formularios con archivos

--- app.js
+++ app.js
@@ -8,29 +8,52 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var mysql = require("mysql");
+var multer = require('multer');
 
 // Importar módulos locales
-var imageRouter = requires("./routers/imageRouter");
+var databse = require("./database");
+var photoRouter = require("./routes/photo-router");
 
 // Crear nuestra aplicación express y comenzar a configurarla
 var app = express();
 
 // Configurar sistema de logging y conversión de datos
 app.use(logger('dev'));
+
+// Aceptar solicitudes en formato application/json
 app.use(bodyParser.json());
+
+// Aceptar solicitudes en formato application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended : false
+}));
 
 // configurar donde iran nuestros archivos del front
 app.use(express.static(__dirname + '/public'));
 
-// Configurar donde carpeta temporarl donde se guardaran las imágenes
-app.use(express.bodyParser({uploadDir:'./uploads'}));
+// Configurar donde carpeta temporal donde se guardaran las imágenes
+app.use(multer({
+	dest : './uploads/'
+}).single('photo'));
 
+// Configurar las rutas en archivos separados
+app.use('/photos', photoRouter);
 
-// Encender el servidor y comenzar a escuchar solicitudes
-var hostname = 'localhost';
-var port = 3000;
+// Configurar la conexión a la base de datos
+databse.connect(databse.MODE_PRODUCTION, function(err) {
+	if (err) {
+		// Si hubo errores cerrar la aplicación
+		console.log('Unable to connect to MySQL.');
+		process.exit(1)
+	} else {
+		// Si no hubo errores encender el servidor y comenzar a escuchar
+		// solicitudes
+		var hostname = 'localhost';
+		var port = 3000;
 
-app.listen(port, hostname, function(){
-	console.log('Servidor corriendo en http://'+hostname+':'+port+'/');
+		app.listen(port, hostname, function() {
+			console.log('Servidor corriendo en http://' + hostname + ':' + port
+					+ '/');
+		});
+	}
 });
---
+++ sql-scripts/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `aplicacion_social`.`photos` (
+  `photo_id` INT NOT NULL AUTO_INCREMENT,
+  `author` VARCHAR(45) NULL,
+  `place` VARCHAR(45) NULL,
+  `photo_path` VARCHAR(100) NOT NULL,
+  `updload_date` DATETIME NOT NULL,
+  PRIMARY KEY (`photo_id`));
